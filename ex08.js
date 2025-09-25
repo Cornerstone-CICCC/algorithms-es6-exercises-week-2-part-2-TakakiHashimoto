45218ebf28b19705b47ffbb7d12eeb462e60f880
gooko <takakihashimotogoo@gmail.com>
2025-09-24 20:06:36 -0700

    completed ex08

--- ex08.js
+++ ex08.js
@@ -1,3 +1,8 @@
 // Join Exercise
-// Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
+// Instructions: Create an array of at least five different words as strings. 
+// Write code to join all the words into a single string, with each word separated by a space.
+
+const words = ["this", "is", "random", "word", "plane", "steak", "human"];
+const joinedWord = words.join(" ");
+console.log(joinedWord);
 
