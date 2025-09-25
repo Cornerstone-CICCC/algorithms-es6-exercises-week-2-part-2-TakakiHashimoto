50b1bbde28b232a7ded667838782c417f55696bc
gooko <takakihashimotogoo@gmail.com>
2025-09-24 20:01:45 -0700

    completed ex06

--- ex06.js
+++ ex06.js
@@ -1,3 +1,10 @@
 // Every Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if every number in the array is positive.
 
+const numbers = [1, 2, 3, 44, 2, 5, 9, 10, 11, 2222, -1];
+
+const isPositive = numbers.every(num => {
+    return num > 0;
+})
+
+console.log(isPositive);
