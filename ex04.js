a822ce8ba08bd13e2aa473d393b869614b255ff3
gooko <takakihashimotogoo@gmail.com>
2025-09-24 19:53:29 -0700

    completed ex04

--- ex04.js
+++ ex04.js
@@ -1,3 +1,12 @@
 // Includes Exercise
-// Instructions: Create an array of at least five different fruits as strings. Write code to check if the array includes the fruit "apple".
+// Instructions: Create an array of at least five different fruits as strings. 
+// Write code to check if the array includes the fruit "apple".
+
+const fruits = ["apple", "banana", "strawberry", "kiwi", "orange"];
+
+if (fruits.includes("apple")){
+    console.log("apple included");
+} else {
+    console.log("apple not included");
+}
 
